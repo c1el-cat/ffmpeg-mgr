6b1991ed0ea353b242654dc0c7f3c86968919011
yuk1_1g0t <yuk1_1g0t@MAIN-1G0T>
2025-06-10 19:20:44 +0900

    プロジェクト ファイルを追加します。

---
+++ ffmpeg-mgr/ffmpeg-mgr.Designer.vb
@@ -0,0 +1,43 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mgr
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' mgr
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Black
+        ClientSize = New Size(10, 10)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "mgr"
+        ShowIcon = False
+        ShowInTaskbar = False
+        StartPosition = FormStartPosition.Manual
+        Text = "mgr"
+        TransparencyKey = Color.Black
+        ResumeLayout(False)
+    End Sub
+
+End Class
---
+++ ffmpeg-mgr/ffmpeg-mgr.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class mgr
+    <DllImport("user32.dll")>
+    Private Shared Function SetWindowLong(hWnd As IntPtr, nIndex As Integer, dwNewLong As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetWindowLong(hWnd As IntPtr, nIndex As Integer) As Integer
+    End Function
+
+    Private Const GWL_EXSTYLE As Integer = -20
+    Private Const WS_EX_TOOLWINDOW As Integer = &H80
+
+    Private Sub mgr_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim exStyle As Integer = GetWindowLong(Me.Handle, GWL_EXSTYLE)
+        SetWindowLong(Me.Handle, GWL_EXSTYLE, exStyle Or WS_EX_TOOLWINDOW)
+    End Sub
+End Class
